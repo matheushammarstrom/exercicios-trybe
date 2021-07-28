51a27b98f012b1e1e1c154a678c8d65a446d4c9a
Matheus <matheusfelippeh@gmail.com>
2021-07-28 17:58:11 -0300

    remove exercicios errados

--- Fundamentos/bloco4/dia1-javascript-primeiros-passos/exercicio1/scripts.js
+++
@@ -1,9 +0,0 @@
-const myName = "Matheus";
-const birthCity = "Sao Paulo";
-var birthYear = 2002;
-birthYear = 2030;
-birthCity = "Itapetninga";
-
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
--- Fundamentos/bloco4/dia1-javascript-primeiros-passos/exercicio2/script.js
+++
@@ -1,6 +0,0 @@
-let base = 5;
-let altura = 8;
-let perimetro;
-perimetro = base*2 + altura*2
-
-console.log(perimetro);
