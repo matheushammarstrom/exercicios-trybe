f9e2c242fdffaef33cf0762f52a2ea2a20798c18
Matheus <matheusfelippeh@gmail.com>
2021-07-28 15:20:28 -0300

    exercicio1

---
+++ Fundamentos/bloco4/dia1-javascript-primeiros-passos/exercicio1/scripts.js
@@ -0,0 +1,9 @@
+const myName = "Matheus";
+const birthCity = "Sao Paulo";
+var birthYear = 2002;
+birthYear = 2030;
+birthCity = "Itapetninga";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
