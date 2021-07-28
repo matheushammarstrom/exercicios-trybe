4486c1f3c6e683bbd6b8f24b0c42e8b8f65c7d0b
Matheus <matheusfelippeh@gmail.com>
2021-07-28 18:13:56 -0300

    adiciona exercicio3

---
+++ Fundamentos/bloco4/dia1-javascript-primeiros-passos/exercicio3_maior_tres_numeros.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b = 2;
+let c = 5;
+
+if(a > b && a > c){
+    console.log(a);
+}
+else if (b > a && b > c){
+    console.log(b);
+}
+else if (c > a && c > b){
+    console.log(c);
+}
