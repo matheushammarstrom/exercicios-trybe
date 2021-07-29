000e4290b1a7495dd6440a16a769ddd3260ac2c2
Matheus <matheusfelippeh@gmail.com>
2021-07-29 18:37:37 -0300

    adiciona exercicio8

---
+++ Fundamentos/bloco4/dia2-javascript-array-e-loop-for/exercicio8.js
@@ -0,0 +1,8 @@
+//Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numbers = [];
+
+for(let index = 1; index < 26; index ++){
+    numbers.push(index);
+}
+console.log(numbers);
