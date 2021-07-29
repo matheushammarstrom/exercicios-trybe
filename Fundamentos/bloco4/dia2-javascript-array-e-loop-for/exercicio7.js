5bf6058a08fc3aca1adfc3ef31c68935b31f20b3
Matheus <matheusfelippeh@gmail.com>
2021-07-29 18:34:23 -0300

    adiciona exercicio7

---
+++ Fundamentos/bloco4/dia2-javascript-array-e-loop-for/exercicio7.js
@@ -0,0 +1,14 @@
+//Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+//Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let smallestNumber = numbers [0];
+
+for(index = 0; index < numbers.length; index ++){
+    if(smallestNumber > numbers[index]){
+        smallestNumber = numbers[index];
+    }
+}
+console.log("O menor numero é : ", smallestNumber)
