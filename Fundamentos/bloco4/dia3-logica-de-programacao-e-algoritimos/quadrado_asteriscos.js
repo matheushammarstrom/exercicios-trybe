dffd3a08536f936617779a6026a5eb2d5f591ab3
Matheus <matheusfelippeh@gmail.com>
2021-07-30 17:38:21 -0300

    adiciona quadrado_asteriscos.js

---
+++ Fundamentos/bloco4/dia3-logica-de-programacao-e-algoritimos/quadrado_asteriscos.js
@@ -0,0 +1,12 @@
+let n = 10;
+let line = "";
+let symbol = "*"
+
+for(let index = 0; index < n; index += 1){
+    line = line + symbol;
+}
+for(let index2 = 0; index2 < n; index2 += 1){
+    
+        console.log(line)
+    
+}
