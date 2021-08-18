e5aac3c4516429984c45fe3edfc02ae9a0ddde89
Matheus <matheusfelippeh@gmail.com>
2021-08-18 20:54:54 -0300

    add preventDefault

--- Fundamentos/bloco6_html_e_css_forms_flexbox_e_responsive/script.js
+++ Fundamentos/bloco6_html_e_css_forms_flexbox_e_responsive/script.js
@@ -1,8 +1,14 @@
 var states =['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
-
+const button = document.querySelector('#submit')
 var stateList = document.querySelector('.state-list')
+
 for(let index =0; index < states.length; index += 1){
     let newState = document.createElement('option')
     newState.innerHTML = states[index]
     stateList.appendChild(newState)
 }
+
+// function getForms(event){
+//     event.preventDefault();
+// }
+// button.addEventListener('click', getForms)
