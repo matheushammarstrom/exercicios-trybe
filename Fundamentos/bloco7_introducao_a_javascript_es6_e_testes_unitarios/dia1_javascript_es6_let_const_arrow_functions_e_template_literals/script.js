ae8bdc62c0038b229c3bd120097031b3266b5c1c
Matheus <matheusfelippeh@gmail.com>
2021-08-27 18:58:56 -0300

    add exercise 1.2

--- Fundamentos/bloco7_introducao_a_javascript_es6_e_testes_unitarios/dia1_javascript_es6_let_const_arrow_functions_e_template_literals/script.js
+++ Fundamentos/bloco7_introducao_a_javascript_es6_e_testes_unitarios/dia1_javascript_es6_let_const_arrow_functions_e_template_literals/script.js
@@ -1,3 +1,4 @@
+//Exercise 1
 const test =  (escopo)=> {
     if (escopo === true) {
       var ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
@@ -11,3 +12,15 @@ const test =  (escopo)=> {
   }
 
   test(true);
+
+  //Exercise 2
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  oddsAndEvens.sort((a,b)=>{
+      return a - b;
+  })
+
+  // Seu código aqui.
+
+  console.log(oddsAndEvens); // 
+
+
