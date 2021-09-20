5dda0d3b11cb9f63e2556a201508cf7b9ea18fc5
Matheus <matheusfelippeh@gmail.com>
2021-09-20 00:00:28 -0300

    add totalSum function and results of the promise

--- Fundamentos/bloco9-javascript-assincrono-e-promises/dia2-javascript-promises/exercise2/script.js
+++ Fundamentos/bloco9-javascript-assincrono-e-promises/dia2-javascript-promises/exercise2/script.js
@@ -4,4 +4,11 @@ const getNumbers = new Promise((resolve, reject) =>{
     const randomNumber = Math.floor(Math.random()*50 + 1);
     numbersArray.push(randomNumber**2);
   }
+  const totalSum = numbersArray.reduce(((acc, curr)=> acc + curr),0);
+  if(totalSum < 8000){
+    return resolve()
+  }
+  reject()
 })
+.then(()=>console.log('Promise Resolvida'))
+.catch(()=> console.log('Promise rejeitada'))
